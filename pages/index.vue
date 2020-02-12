3df9114f705b7bdf2ab8b948914ed74e00bec1da
r-kojima <rkojima0213@gmail.com>
2020-02-12 20:13:53 +0900

    全体見た目調整 ＆ ディレクトリ名変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
+  <v-row>
+    <v-col cols="12">
       <v-card>
         <v-card-title class="headline">
           プロフィール
@@ -12,8 +12,8 @@
           </p>
         </v-card-text>
       </v-card>
-    </v-flex>
-    <v-flex xs12 sm8 md6>
+    </v-col>
+    <v-col cols="12">
       <v-card>
         <v-card-title class="headline">
           Skills
@@ -30,8 +30,8 @@
           </template>
         </v-card-text>
       </v-card>
-    </v-flex>
-    <v-flex xs12 sm8 md6>
+    </v-col>
+    <v-col cols="12">
       <v-card>
         <v-card-title class="headline">
           職務経歴
@@ -40,68 +40,79 @@
           なんかいろいろ
         </v-card-text>
       </v-card>
-    </v-flex>
-    <v-flex xs12 sm8 md6>
+    </v-col>
+    <v-col cols="12">
       <v-card>
         <v-card-title class="headline">
-          個人開発(Personal Works)
+          個人開発
         </v-card-title>
-        <v-card-text>
-          なんかいろいろ
-        </v-card-text>
+
+        <v-row class="d-flex flex-row">
+          <v-card
+            v-for="personalWork in personalWorks"
+            :key="personalWork.title"
+            class="mx-auto"
+            color="brown darken-3"
+            max-width="344"
+          >
+            <v-img
+              :src="personalWork.imageUrl"
+              :alt="personalWork.title"
+              height="200px"
+              bor
+            ></v-img>
+
+            <v-card-title>
+              <v-avatar v-if="personalWork.iconUrl" color="black" size="36">
+                <v-img :src="personalWork.iconUrl"></v-img>
+              </v-avatar>
+
+              {{ personalWork.title }}
+            </v-card-title>
+
+            <v-card-text>
+              <span v-html="personalWork.description"></span>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn v-if="personalWork.links.app" text>
+                <a :href="personalWork.links.app" target="_blank">App link</a>
+              </v-btn>
+
+              <v-btn v-if="personalWork.links.github" color="purple" text>
+                <a :href="personalWork.links.github" target="_blank">GitHub</a>
+              </v-btn>
+
+              <v-spacer></v-spacer>
+
+              <v-btn
+                v-if="personalWork.expansionDetail"
+                icon
+                @click="personalWork.showDetail = !personalWork.showDetail"
+              >
+                <v-icon>{{
+                  personalWork.showDetail
+                    ? 'mdi-chevron-up'
+                    : 'mdi-chevron-down'
+                }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+
+            <v-expand-transition v-if="personalWork.expansionDetail">
+              <div v-show="personalWork.showDetail">
+                <v-divider></v-divider>
+
+                <v-card-text>
+                  {{ personalWork.expansionDetail }}
+                </v-card-text>
+              </div>
+            </v-expand-transition>
+          </v-card>
+        </v-row>
       </v-card>
+    </v-col>
 
-      <template v-for="personalWork in personalWorks">
-        <v-card :key="personalWork.title" class="mx-auto" max-width="344">
-          <v-img
-            :src="personalWork.imageUrl"
-            :alt="personalWork.title"
-            height="200px"
-          ></v-img>
-
-          <v-card-title>
-            {{ personalWork.title }}
-          </v-card-title>
-
-          <v-card-text>
-            <span v-html="personalWork.description"></span>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn v-if="personalWork.links.app" text>
-              <a :href="personalWork.links.app" target="_blank">App link</a>
-            </v-btn>
-
-            <v-btn v-if="personalWork.links.github" color="purple" text>
-              <a :href="personalWork.links.github" target="_blank">GitHub</a>
-            </v-btn>
-
-            <v-spacer></v-spacer>
-
-            <v-btn
-              icon
-              @click="personalWork.showDetail = !personalWork.showDetail"
-            >
-              <v-icon>{{
-                personalWork.showDetail ? 'mdi-chevron-up' : 'mdi-chevron-down'
-              }}</v-icon>
-            </v-btn>
-          </v-card-actions>
-
-          <v-expand-transition>
-            <div v-show="personalWork.showDetail">
-              <v-divider></v-divider>
-
-              <v-card-text>
-                {{ personalWork.expansionDetail }}
-              </v-card-text>
-            </div>
-          </v-expand-transition>
-        </v-card>
-      </template>
-    </v-flex>
-
-    <v-flex xs12 sm8 md6>
+    <v-col cols="12">
       <v-card>
         <v-card-title class="headline">
           コンタクト
@@ -115,8 +126,8 @@
           </template>
         </v-card-text>
       </v-card>
-    </v-flex>
-  </v-layout>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -142,8 +153,8 @@ export default {
         },
         {
           name: 'email',
-          account: '',
-          url: ''
+          account: 'rkojima0213@gmail.com',
+          url: 'mailto:rkojima0213@gmail.com'
         }
       ],
       skills: [
@@ -161,19 +172,33 @@ export default {
       personalWorks: [
         {
           title: 'simpleGestures',
-          imageUrl: '/works/thumb/simpleGestures.png',
+          iconUrl: '/works/icons/simpleGestures.png',
+          imageUrl: '/works/thumbs/simpleGestures.png',
           description:
             '<div>JavaScript / Chrome extension</div>\n' +
             'マウスジェスチャ機能を提供するためのGoogle Chrome拡張機能です。\n' +
             '軽量/簡単な設定・操作を目指しています。',
           showDetail: false,
           expansionDetail:
-            'Google Chromeでマウスジェスチャを行うための拡張機能です。\n' +
-            '軽量/簡単な設定・操作を目指しています。',
+            'Chrome Web Storeに公開しており、7,000人のユーザーに利用いただいています。',
           links: {
             app:
               'https://chrome.google.com/webstore/detail/flfminafiamnggnldfpilnfnmbgmiegn',
             github: 'https://github.com/RyutaKojima/simpleGestures'
+          }
+        },
+        {
+          title: 'MySQLSchemaDoc',
+          iconUrl: '',
+          imageUrl: '/works/thumbs/MySQLSchemaDoc.png',
+          description:
+            '<div>MySQL / xsltproc / JavaScript / css</div>\n' +
+            'MySQLのDDLからテーブル定義書を作成するためのツールです。',
+          showDetail: false,
+          expansionDetail: '',
+          links: {
+            app: 'https://ryutakojima.github.io/MySQLSchemaDoc/',
+            github: 'https://github.com/RyutaKojima/MySQLSchemaDoc'
           }
         }
       ]
